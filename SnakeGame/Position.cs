61cefe70dfde91373b8be3a69da000c88be80d89
Smack1990 <johansson.t.markus@gmail.com>
2024-10-25 12:36:03 +0200

    Add project files.

---
+++ SnakeGame/Position.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame;
+public class Position
+{
+    public Position(int row, int column)
+    {
+        Row = row;
+        Col = column;
+    }
+
+    public int Row { get; }
+    public int Col { get; }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Position position &&
+               Row == position.Row &&
+               Col == position.Col;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Row, Col);
+    }
+
+    public Position Translate(Direction dir)
+    {
+        return new Position(Row + dir.RowOffset, Col + dir.ColumnOffset);
+    }
+
+    public static bool operator ==(Position left, Position right)
+    {
+        return EqualityComparer<Position>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(Position left, Position right)
+    {
+        return !(left == right);
+    }
+}
